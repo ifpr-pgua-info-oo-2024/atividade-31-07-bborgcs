611bd3c76dc12179db861a7557448bc04a4a9ed8
gustavoassunc <gabrielmachado.f29@gmail.com>
2024-08-14 11:43:23 -0300

    alteração, aula 14/08, herança

--- Aluno.java
+++ Aluno.java
@@ -1,83 +1,29 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.text.ParseException;
 
-public class Aluno {
+public class Aluno extends Pessoa{
 
-    private String nome;
     private String nivel;
-    private Date idade; //data de nascimento
-    private String genero;
-    private double altura;
-    private double peso;
-
-    public Aluno (String nome, String nivel, Date idade, String genero, double altura, double peso) {
-        this.nome = nome;
+    
+    public Aluno (String nome, String nivel, String idade, String genero, double altura, double peso) throws ParseException {
+        super(nome, idade, genero, altura, peso);
         this.nivel = nivel;
-        this.idade = idade;
-        this.genero = genero;
-        this.altura = altura;
-        this.peso = peso;
     }
 
-    public Aluno (String nome, String nivel, Date idade) {
+    public Aluno (String nome, String nivel, String idade) throws ParseException {
         this(nome, nivel, idade, "", 0 , 0);
-    }
-
-    public String getNome() {
-        return this.nome;
     }
 
     public String getNivel() {
         return this.nivel;
     }
 
-    public Date getIdade() {
-        return this.idade;
-    }
-
-    public String getGenero() {
-        return this.genero;
-    }
-
-    public double getAltura() {
-        return this.altura;
-    }
-
-    public double getPeso() {
-        return this.peso;
-    }
-
-
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public void setNivel(String nivel) {
         this.nivel = nivel;
     }
 
-    public void setIdade(Date idade) {
-        this.idade = idade;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
-    }
-
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
     @Override
     public String toString() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        return "Aluno {nome:" + nome + ", nivel:" + nivel + ", data de nascimento:" + simpleDateFormat.format(idade) + ", genero:" + genero + ", altura:"
-                + altura + ", peso:" + peso + "}";
+        return super.toString()+" nivel:" + nivel+ "}";
     }
 
     
--- Main.java
+++ Main.java
@@ -11,25 +11,24 @@ public class Main {
     
     public static void main(String[] args) throws ParseException {
         Academia a = new Academia("World", "Rua Tókio, 472", "41 9999-9999");
-
        System.out.println(a.getNome()+" "+a.getEndereco()+" "+a.getTelefone());
+       
        a.setNome("Bio Aventura");
        a.setEndereco("Av Bento Munhoz da Rocha Neto, 12");
        a.setTelefone("41 3333-3333");
        System.out.println(a.getNome()+" "+a.getEndereco()+" "+a.getTelefone());
 
        
-       a.alunos.add(new Aluno("Maria", "iniciante", convertStringtoDate("10/12/2007")));
-       a.alunos.add(new Aluno("Kauany", "avançado", convertStringtoDate("17/04/2007"), "feminino", 1.57, 45.00));
-       a.alunos.add(new Aluno("João", "intermediário", convertStringtoDate("16/09/1999")));
+       a.alunos.add(new Aluno("Maria", "iniciante", "10/12/2007"));
+       a.alunos.add(new Aluno("Kauany", "avançado", "17/04/2007", "feminino", 1.57, 45.00));
+       a.alunos.add(new Aluno("João", "intermediário", "16/09/1999"));
        
        for (Aluno aluno : a.getAlunos()) {
            System.out.println(aluno.toString());
        }
 
-       Academia b = new Academia("caopi", "lgbhfd, 77", "41 3434-5677");
-
        System.out.println("\n\n\n     OBJETO 2 ");
+       Academia b = new Academia("caopi", "lgbhfd, 77", "41 3434-5677");
        System.out.println(b.getNome()+" "+ b.getEndereco()+" "+b.getTelefone());
 
        b.setNome("dominio");
@@ -38,9 +37,9 @@ public class Main {
        
        System.out.println(b.getNome()+" "+b.getEndereco()+" "+b.getTelefone());
 
-       b.alunos.add(new Aluno("Clara", "iniciante", convertStringtoDate("30/07/1987")));
-       b.alunos.add(new Aluno("Marcela", "avançado", convertStringtoDate("17/05/2000"), "feminino", 1.57, 45.00));
-       b.alunos.add(new Aluno("Paulo", "intermediário", convertStringtoDate("11/11/2011")));
+       b.alunos.add(new Aluno("Clara", "iniciante", "30/07/1987"));
+       b.alunos.add(new Aluno("Marcela", "avançado", "17/05/2000", "feminino", 1.57, 45.00));
+       b.alunos.add(new Aluno("Paulo", "intermediário", "11/11/2011"));
        
        for (Aluno aluno : b.getAlunos()) {
            System.out.println(aluno.toString());
